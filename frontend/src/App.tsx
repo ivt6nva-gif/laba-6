893b520deb9746b679b4dee9a9d3b0addf9aa616
ivt6nva-gif <ivt6_nva@student.ztu.edu.ua>
2026-01-02 20:42:12 +0200

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type { FC } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Registration } from "./pages/Registration";
+import { Login } from "./pages/Auth";
+import { DashboardPage } from "./pages/Dashboard";
+import { NotFound } from "./pages/404";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App: FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+  <>
+  <main>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </main>
+  </>);};
+  
+export default App;
